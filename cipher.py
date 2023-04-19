dbd6c42409a1a963ade3c78583ba07e18ee5767f
Daniil-7 <yas66yelchili@mail.ru>
2023-04-19 12:18:05 +0300

    new console.py

--- cipher.py
+++ cipher.py
@@ -3,13 +3,13 @@ import pyAesCrypt
 
 
 class Crypto:
-    def __crypt(self, file, password, buffer_size, logger):
+    def file_crypt(self, file, password, buffer_size=524288, logger=True):
         pyAesCrypt.encryptFile(str(file), str(file) + ".crp", password, buffer_size)
         if logger:
             print(f"[Encrypt] '{str(file)}.crp'")
         os.remove(file)
 
-    def __decrypt(self, file, password, buffer_size, logger):
+    def file_decrypt(self, file, password, buffer_size=524288, logger=True):
         pyAesCrypt.decryptFile(
             str(file), str(os.path.splitext(file)[0]), password, buffer_size
         )
@@ -17,24 +17,24 @@ class Crypto:
             print(f"[Decrypt] '{str(os.path.splitext(file)[0])}'")
         os.remove(file)
 
-    def walk_crypt(
-        self, directory, password, buffer_size=524288, logger=True, system_logger=True
+    def directory_crypt(
+        self, directory, password, buffer_size=524288, logger=True, title_logger=True
     ):
-        if logger and system_logger:
+        if logger and title_logger:
             print("[***Crypto***]")
         if logger:
             print(f"[Directory] '{directory}'")
         for name in os.listdir(directory):
             path = os.path.join(directory, name)
             if os.path.isfile(path):
-                self.__crypt(path, password, buffer_size, logger)
+                self.file_crypt(path, password, buffer_size, logger)
             else:
-                self.walk_crypt(path, password, buffer_size, logger, False)
+                self.directory_crypt(path, password, buffer_size, logger, False)
 
-    def walk_decrypt(
-        self, directory, password, buffer_size=524288, logger=True, system_logger=True
+    def directory_decrypt(
+        self, directory, password, buffer_size=524288, logger=True, title_logger=True
     ):
-        if logger and system_logger:
+        if logger and title_logger:
             print("[***Derypto***]")
         if logger:
             print(f"[Directory] '{directory}'")
@@ -42,8 +42,8 @@ class Crypto:
             path = os.path.join(directory, name)
             if os.path.isfile(path):
                 try:
-                    self.__decrypt(path, password, buffer_size, logger)
+                    self.file_decrypt(path, password, buffer_size, logger)
                 except Exception:
                     pass
             else:
-                self.walk_decrypt(path, password, buffer_size, logger, False)
+                self.directory_decrypt(path, password, buffer_size, logger, False)
--- console.py
+++ console.py
@@ -1,16 +1,42 @@
-from crypto1 import Crypto
+from cipher import Crypto
+import os, click
+
+
+crypt = Crypto()
+
+
+@click.command()
+@click.argument('crypt_or_decrypt')
+@click.argument('path')
+@click.argument('password')
+def main(crypt_or_decrypt, path, password):
+    """
+    Encryptor and decryptor of files and directories\n
+    Attention\n
+    All information is provided for informational purposes only.
+    The author is not responsible for any possible harm caused by the materials of this program.\n
+    Instruction manual\n
+    You need to enter three arguments:\n
+    1. crypt_or_decrypt - Enter:
+    "crypt" - if you want to encrypt
+    "decrypt" - if you want to decrypt\n
+    2. path - Enter the path to the directory or file\n
+    3. password - Enter the password
+    """
+    print("[---Program Tech Crypto---]")
+    if crypt_or_decrypt != "crypt" and crypt_or_decrypt != "decrypt":
+        print("Wrong input! Enter crypt or decrypt!")
+    if not os.path.exists(path):
+        print("The path to the directory or file is not correct. Enter an existing path!")
+    if crypt_or_decrypt == "crypt" and os.path.isdir(path):
+        crypt.directory_crypt(path, password)
+    if crypt_or_decrypt == "decrypt" and os.path.isdir(path):
+        crypt.directory_decrypt(path, password)
+    if crypt_or_decrypt == "crypt" and os.path.isfile(path):
+        crypt.file_crypt(path, password)
+    if crypt_or_decrypt == "decrypt" and os.path.isfile(path):
+        crypt.file_decrypt(path, password)
+
 
 if __name__ == "__main__":
-    print("[---Program Tech Crypto---]")
-    crypt_or_decrypt = str(
-        input(
-            "Введите:\ncrypt - если вы хотите зашифровать\ndecrypt - если хотите расшифровать\n>> "
-        )
-    )
-    path_user = str(input("Введите путь к директории:\n>> "))
-    password_user = str(input("Введите пароль:\n>> "))
-    crypt = Crypto()
-    if crypt_or_decrypt == "crypt":
-        crypt.walk_crypt(path_user, password_user)
-    if crypt_or_decrypt == "decrypt":
-        crypt.walk_decrypt(path_user, password_user)
+    main()
